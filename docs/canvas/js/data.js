fe6b26c819d8e80c55b44a7da4a856caba6671c4
shinoyakame <shinoyakame02@gmail.com>
2025-11-11 06:07:05 +0700

    rearrange

--- docs/canvas/js/data.js
+++ docs/canvas/js/data.js
@@ -174,6 +174,21 @@ var characterInfo = [{
     power: 112000
 }, {
     id: ++characterIndex,
+    name: 'Snicker Magpie',
+    imageUrl: './img/character/073.png',
+    power: 32800
+}, {
+    id: ++characterIndex,
+    name: 'Sabi Rabbit',
+    imageUrl: './img/character/046.png',
+    power: 167800
+}, {
+    id: ++characterIndex,
+    name: 'Twink Rabbit',
+    imageUrl: './img/character/105.png',
+    power: 92500
+}, {
+    id: ++characterIndex,
     name: 'Frost Polarbear',
     imageUrl: './img/character/009.png',
     power: 285000
@@ -199,14 +214,104 @@ var characterInfo = [{
     power: 36000
 }, {
     id: ++characterIndex,
-    name: 'Golgol Slime',
-    imageUrl: './img/character/016.png',
-    power: 72000
-}, {
-    id: ++characterIndex,
     name: 'Sarada Polarbear',
     imageUrl: './img/character/017.png',
     power: 112000
+}, {
+    id: ++characterIndex,
+    name: 'Orgey Pig',
+    imageUrl: './img/character/027.png',
+    power: 32400
+}, {
+    id: ++characterIndex,
+    name: 'Jolly Elephant',
+    imageUrl: './img/character/028.png',
+    power: 11000
+}, {
+    id: ++characterIndex,
+    name: 'Feel Rabbit',
+    imageUrl: './img/character/029.png',
+    power: 74000
+}, {
+    id: ++characterIndex,
+    name: 'Flame Cat',
+    imageUrl: './img/character/030.png',
+    power: 34000
+}, {
+    id: ++characterIndex,
+    name: 'Ore Panda',
+    imageUrl: './img/character/031.png',
+    power: 121000
+}, {
+    id: ++characterIndex,
+    name: 'Malee Squirrel',
+    imageUrl: './img/character/035.png',
+    power: 56500
+}, {
+    id: ++characterIndex,
+    name: 'Shelter Bull',
+    imageUrl: './img/character/036.png',
+    power: 33800
+}, {
+    id: ++characterIndex,
+    name: 'Dream Sheep',
+    imageUrl: './img/character/041.png',
+    power: 40000
+}, {
+    id: ++characterIndex,
+    name: 'Kyle Duck',
+    imageUrl: './img/character/042.png',
+    power: 9000
+}, {
+    id: ++characterIndex,
+    name: 'Remedy Rabbit',
+    imageUrl: './img/character/047.png',
+    power: 30000
+}, {
+    id: ++characterIndex,
+    name: 'Radow Magpie',
+    imageUrl: './img/character/048.png',
+    power: 141200
+}, {
+    id: ++characterIndex,
+    name: 'Rampert Tiger',
+    imageUrl: './img/character/106.png',
+    power: 94000
+}, {
+    id: ++characterIndex,
+    name: 'Caren Eagle',
+    imageUrl: './img/character/107.png',
+    power: 57000
+}, {
+    id: ++characterIndex,
+    name: 'Rosalia Rat',
+    imageUrl: './img/character/108.png',
+    power: 57500
+}, {
+    id: ++characterIndex,
+    name: 'Apollo Rabbit',
+    imageUrl: './img/character/059.png',
+    power: 295000
+}, {
+    id: ++characterIndex,
+    name: 'Hades Rabbit',
+    imageUrl: './img/character/060.png',
+    power: 400000
+}, {
+    id: ++characterIndex,
+    name: 'Shirou Sparrow',
+    imageUrl: './img/character/063.png',
+    power: 168200
+}, {
+    id: ++characterIndex,
+    name: 'Ghost Loris',
+    imageUrl: './img/character/074.png',
+    power: 13200
+}, {
+    id: ++characterIndex,
+    name: 'Cherry Woodpecker',
+    imageUrl: './img/character/075.png',
+    power: 15400
 }, {
     id: ++characterIndex,
     name: 'Phiral Penguin',
@@ -244,49 +349,9 @@ var characterInfo = [{
     power: 9500
 }, {
     id: ++characterIndex,
-    name: 'Orgey Pig',
-    imageUrl: './img/character/027.png',
-    power: 32400
-}, {
-    id: ++characterIndex,
-    name: 'Jolly Elephant',
-    imageUrl: './img/character/028.png',
-    power: 11000
-}, {
-    id: ++characterIndex,
-    name: 'Feel Rabbit',
-    imageUrl: './img/character/029.png',
-    power: 74000
-}, {
-    id: ++characterIndex,
-    name: 'Flame Cat',
-    imageUrl: './img/character/030.png',
-    power: 34000
-}, {
-    id: ++characterIndex,
-    name: 'Ore Panda',
-    imageUrl: './img/character/031.png',
-    power: 121000
-}, {
-    id: ++characterIndex,
     name: 'Sica Frog',
     imageUrl: './img/character/032.png',
     power: 7200
-}, {
-    id: ++characterIndex,
-    name: 'Malee Squirrel',
-    imageUrl: './img/character/035.png',
-    power: 56500
-}, {
-    id: ++characterIndex,
-    name: 'Shelter Bull',
-    imageUrl: './img/character/036.png',
-    power: 33800
-}, {
-    id: ++characterIndex,
-    name: 'Veenus Dolphin',
-    imageUrl: './img/ni.png',
-    power: 42000
 }, {
     id: ++characterIndex,
     name: 'Phenia Phoenix',
@@ -294,34 +359,9 @@ var characterInfo = [{
     power: 25000
 }, {
     id: ++characterIndex,
-    name: 'Dream Sheep',
-    imageUrl: './img/character/041.png',
-    power: 40000
-}, {
-    id: ++characterIndex,
-    name: 'Kyle Duck',
-    imageUrl: './img/character/042.png',
-    power: 9000
-}, {
-    id: ++characterIndex,
     name: 'Chimone Chimera',
     imageUrl: './img/character/044.png',
     power: 60000
-}, {
-    id: ++characterIndex,
-    name: 'Sabi Rabbit',
-    imageUrl: './img/character/046.png',
-    power: 167800
-}, {
-    id: ++characterIndex,
-    name: 'Remedy Rabbit',
-    imageUrl: './img/character/047.png',
-    power: 30000
-}, {
-    id: ++characterIndex,
-    name: 'Radow Magpie',
-    imageUrl: './img/character/048.png',
-    power: 141200
 }, {
     id: ++characterIndex,
     name: 'Pearl Tortoise (LD)',
@@ -339,70 +379,15 @@ var characterInfo = [{
     power: 55000
 }, {
     id: ++characterIndex,
-    name: 'Red Rabbit',
-    imageUrl: './img/ni.png',
-    power: 24000
-}, {
-    id: ++characterIndex,
-    name: 'Yellow Rabbit',
-    imageUrl: './img/ni.png',
-    power: 21000
-}, {
-    id: ++characterIndex,
-    name: 'Green Rabbit',
-    imageUrl: './img/ni.png',
-    power: 23500
-}, {
-    id: ++characterIndex,
-    name: 'Blue Rabbit',
-    imageUrl: './img/ni.png',
-    power: 24500
-}, {
-    id: ++characterIndex,
-    name: 'Brown Rabbit',
-    imageUrl: './img/ni.png',
-    power: 20800
-}, {
-    id: ++characterIndex,
-    name: 'Gray Rabbit',
-    imageUrl: './img/ni.png',
-    power: 20500
-}, {
-    id: ++characterIndex,
-    name: 'Apollo Rabbit',
-    imageUrl: './img/character/059.png',
-    power: 295000
-}, {
-    id: ++characterIndex,
-    name: 'Hades Rabbit',
-    imageUrl: './img/character/060.png',
-    power: 400000
-}, {
-    id: ++characterIndex,
     name: 'Lightning Cheetah',
     imageUrl: './img/ni.png',
     power: 130000
-}, {
-    id: ++characterIndex,
-    name: 'Shirou Sparrow',
-    imageUrl: './img/character/063.png',
-    power: 168200
 }, {
     id: ++characterIndex,
     name: 'Lalami Llama',
     imageUrl: './img/character/064.png',
     power: 221000,
     ai: true
-}, {
-    id: ++characterIndex,
-    name: 'Nume Sparrow',
-    imageUrl: './img/character/065.png',
-    power: 750
-}, {
-    id: ++characterIndex,
-    name: 'Charotte Parrot',
-    imageUrl: './img/character/066.png',
-    power: 1050
 }, {
     id: ++characterIndex,
     name: 'Dragon Buffalo',
@@ -430,21 +415,6 @@ var characterInfo = [{
     imageUrl: './img/character/071.png',
     power: 131000,
     ai: true
-}, {
-    id: ++characterIndex,
-    name: 'Snicker Magpie',
-    imageUrl: './img/character/073.png',
-    power: 32800
-}, {
-    id: ++characterIndex,
-    name: 'Ghost Loris',
-    imageUrl: './img/character/074.png',
-    power: 13200
-}, {
-    id: ++characterIndex,
-    name: 'Cherry Woodpecker',
-    imageUrl: './img/character/075.png',
-    power: 15400
 }, {
     id: ++characterIndex,
     name: 'Uka Camel',
@@ -575,40 +545,10 @@ var characterInfo = [{
     power: 375000
 }, {
     id: ++characterIndex,
-    name: 'Cerberus',
-    imageUrl: './img/ni.png',
-    power: 50000
-}, {
-    id: ++characterIndex,
     name: 'Circuit Dog',
     imageUrl: './img/character/104.png',
     power: 222000,
     ai: true
-}, {
-    id: ++characterIndex,
-    name: 'Twink Rabbit',
-    imageUrl: './img/character/105.png',
-    power: 92500
-}, {
-    id: ++characterIndex,
-    name: 'Rampert Tiger',
-    imageUrl: './img/character/106.png',
-    power: 94000
-}, {
-    id: ++characterIndex,
-    name: 'Caren Eagle',
-    imageUrl: './img/character/107.png',
-    power: 57000
-}, {
-    id: ++characterIndex,
-    name: 'Rosalia Rat',
-    imageUrl: './img/character/108.png',
-    power: 57500
-}, {
-    id: ++characterIndex,
-    name: 'Minerva Mink',
-    imageUrl: './img/ni.png',
-    power: 39000
 }, {
     id: ++characterIndex,
     name: 'Behemoth Panther',
@@ -831,19 +771,9 @@ var characterInfo = [{
     power: 1177000
 }, {
     id: ++characterIndex,
-    name: 'White Sheep',
-    imageUrl: './img/ni.png',
-    power: 69000
-}, {
-    id: ++characterIndex,
     name: 'Anubis Jackal',
     imageUrl: './img/character/169.png',
     power: 167200
-}, {
-    id: ++characterIndex,
-    name: 'Bastet Cat',
-    imageUrl: './img/character/170.png',
-    power: 92000
 }, {
     id: ++characterIndex,
     name: 'Vadore Cat',
@@ -859,11 +789,6 @@ var characterInfo = [{
     name: 'Glacier I.Dragon',
     imageUrl: './img/character/176.png',
     power: 67500
-}, {
-    id: ++characterIndex,
-    name: 'Hedro Hydra',
-    imageUrl: './img/ni.png',
-    power: 60200
 }, {
     id: ++characterIndex,
     name: 'Gana Squirrel',
@@ -1000,6 +925,71 @@ var deprecatedList = [{
     imageUrl: './img/character/183.png',
     power: 12200,
     deprecated: true
+}, {
+    id: ++characterIndex,
+    name: 'Minerva Mink',
+    imageUrl: './img/ni.png',
+    power: 39000
+}, {
+    id: ++characterIndex,
+    name: 'Nume Sparrow',
+    imageUrl: './img/character/065.png',
+    power: 750
+}, {
+    id: ++characterIndex,
+    name: 'Charotte Parrot',
+    imageUrl: './img/character/066.png',
+    power: 1050
+}, {
+    id: ++characterIndex,
+    name: 'Red Rabbit',
+    imageUrl: './img/ni.png',
+    power: 24000
+}, {
+    id: ++characterIndex,
+    name: 'Yellow Rabbit',
+    imageUrl: './img/ni.png',
+    power: 21000
+}, {
+    id: ++characterIndex,
+    name: 'Green Rabbit',
+    imageUrl: './img/ni.png',
+    power: 23500
+}, {
+    id: ++characterIndex,
+    name: 'Blue Rabbit',
+    imageUrl: './img/ni.png',
+    power: 24500
+}, {
+    id: ++characterIndex,
+    name: 'Brown Rabbit',
+    imageUrl: './img/ni.png',
+    power: 20800
+}, {
+    id: ++characterIndex,
+    name: 'Gray Rabbit',
+    imageUrl: './img/ni.png',
+    power: 20500
+}, {
+    id: ++characterIndex,
+    name: 'Golgol Slime',
+    imageUrl: './img/character/016.png',
+    power: 72000
+}, {
+    id: ++characterIndex,
+    name: 'Bastet Cat',
+    imageUrl: './img/character/170.png',
+    power: 92000
+}, {
+    id: ++characterIndex,
+    name: 'Hedro Hydra',
+    imageUrl: './img/ni.png',
+    power: 60200
+}, {
+    id: ++characterIndex,
+    name: 'Cerberus',
+    imageUrl: './img/ni.png',
+    power: 50000
 }];
 var extendedInfo = [{
     id: ++characterIndex,
@@ -1056,6 +1046,16 @@ var extendedInfo = [{
     name: 'Samli Rat',
     imageUrl: './img/ni.png',
     power: 15000
+}, {
+    id: ++characterIndex,
+    name: 'Veenus Dolphin',
+    imageUrl: './img/ni.png',
+    power: 42000
+}, {
+    id: ++characterIndex,
+    name: 'White Sheep',
+    imageUrl: './img/ni.png',
+    power: 69000
 }];
 
 function initCharacterInfo() {
